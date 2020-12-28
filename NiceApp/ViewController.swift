31efeb0536846efa5c2553e7b5dd03db69b08bcd
MinSeok Kim <minseoki0818@gmail.com>
2020-12-28 17:14:16 +0900

    Initial Commit

---
+++ NiceApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NiceApp
+//
+//  Created by 김민석 on 2020/12/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
