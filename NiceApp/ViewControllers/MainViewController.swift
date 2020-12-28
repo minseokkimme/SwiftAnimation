744c5921e79618b2ab7707d48b875e1ac0663da2
MinSeok Kim <minseoki0818@gmail.com>
2020-12-28 18:09:58 +0900

    git init

---
+++ NiceApp/ViewControllers/MainViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ViewController.swift
+//  NiceApp
+//
+//  Created by 김민석 on 2020/12/28.
+//
+
+import UIKit
+import Lottie
+
+class MainViewController: UIViewController {
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .black
+        label.textAlignment = .center
+        label.font = UIFont.boldSystemFont(ofSize: 70)
+        label.text = "메인 화면"
+        return label
+    }()
+    
+    let animationView: AnimationView = {
+        
+        let animView = AnimationView(name: "42483-blinking-santa")
+        animView.frame = CGRect(x:0,y:0,width: 400,height: 400)
+        
+        animView.contentMode = .scaleAspectFill
+        return animView
+    }()
+
+    // 뷰가 생성되었을 때
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .black
+        view.addSubview(animationView)
+        animationView.center = view.center
+        
+        // 애니메이션 실행
+        //
+        animationView.play{ (finish) in
+           print("애니메이션이 끝났다.")
+            
+            self.view.backgroundColor = .white
+            self.animationView.removeFromSuperview()
+            self.view.addSubview(self.titleLabel)
+            
+            self.titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            self.titleLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+            self.titleLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        }
+    }
+
+
+}
+
